d22f00878bdc13512017cc44fe136cadd7ea3d83
Азат Ильдарханов <defcon797@gmail.com>
2022-06-28 20:20:53 +0300

    fixed #6

--- src/main/java/ru/netology/Player.java
+++ src/main/java/ru/netology/Player.java
@@ -57,6 +57,18 @@ public class Player {
     /** Метод принимает жанр и возвращает игру этого жанра, в которую играли больше всего
      Если в игры этого жанра не играли, возвращается null */
     public Game mostPlayerByGenre(String genre) {
-        return null;
+        Game result = null;
+        for (Game game : playedTime.keySet()) {
+            if (game.getGenre().equals(genre)) {
+                if (result == null) {
+                    result = game;
+                } else {
+                    if (playedTime.get(game) > playedTime.get(result)) {
+                        result = game;
+                    }
+                }
+            }
+        }
+        return result;
     }
 }
